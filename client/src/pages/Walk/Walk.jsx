889ae3acb6280db9ed2d32aaac752d4308fc43db
Saelens Aurelien <aureliensaelens0@gmail.com>
2022-01-27 15:17:47 +0100

    update in progress

--- client/src/pages/Walk/Walk.jsx
+++ client/src/pages/Walk/Walk.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Parallax from '../../components/Parallax/Parallax';
 import Topbar from '../../components/Topbar/Topbar';
 import Fetchapi from '../../components/FetchApi/Fetchapi';
-import GoogleApiWrapper from '../../GoogleApiWrapper';
 function Walk() {
   return (
     <div className="walk-content">
+      <Topbar />
       <Fetchapi/>
     </div>
   );
