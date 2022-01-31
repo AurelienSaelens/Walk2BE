32c16b132985ac4cd18e17c27ad99e4699e6be0d
Saelens Aurelien <aureliensaelens0@gmail.com>
2022-01-31 16:55:05 +0100

    upgrade

--- client/src/pages/Walk/Walk.jsx
+++ client/src/pages/Walk/Walk.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Topbar from '../../components/Topbar/Topbar';
 import Fetchapi from '../../components/FetchApi/Fetchapi';
+import Map from '../../components/Map/Map';
+
 function Walk() {
   return (
     <div className="walk-content">
-      <Topbar />
-      <Fetchapi/>
+    <Fetchapi />
+      <Map />
     </div>
   );
 }
