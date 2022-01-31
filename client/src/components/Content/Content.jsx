90c892077806b86a038cddc5adf9b88eff0038e0
Saelens Aurelien <aureliensaelens0@gmail.com>
2022-01-31 16:53:38 +0100

    delete info

--- client/src/components/Content/Content.jsx
+++ client/src/components/Content/Content.jsx
@@ -16,7 +16,7 @@ function Content() {
 
   return (
     <div className="Content">
-      <p>Walk2BE is a fantastic platform dedicated to short and long walks, by bike or on foot, <br></br>  it will make everyone <span class="yellow">happy</span>.<br></br>To find the ride of your <span className="yellow">dreams</span>, you just have to register and find your meeting point! <br></br> This platform is free and will remain so. </p>
+      <p>Walk2BE is a fantastic platform dedicated to short and long walks, by bike or on foot, <br></br>  it will make everyone <span class="yellow">happy</span>.<br></br>To find the ride of your <span className="yellow">dreams</span>, you just have to <span class="yellow">register</span> and find your meeting point! <br></br> This platform is free and will remain so. </p>
     </div>
 
   );
