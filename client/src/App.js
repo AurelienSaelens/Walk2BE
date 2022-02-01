a1b9b3c621089ea5e33534455792353923646c03
Saelens Aurelien <aureliensaelens0@gmail.com>
2022-02-01 18:18:12 +0100

    Adding a drag and drop slider

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import './components/Parallax/parallax.css';
 import Home from './pages/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import app from './app.css'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
-import Register from './pages/Register/Register';
-import { useContext, useState } from "react";
+import { useContext} from "react";
 import { AuthContext } from './Context/AuthContext';
 import Walk from './pages/Walk/Walk';
 import Login from './pages/Login/Login';
@@ -16,19 +14,12 @@ import Login from './pages/Login/Login';
 
 
 function App() {
-  const { user } = useContext(AuthContext);
-
   return (
     <>
     <Router>
       <Switch>
         <Route exact path="/">
        <Home /> 
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/register">
         </Route>
         <Route exact path="/walk">
         <Walk />
