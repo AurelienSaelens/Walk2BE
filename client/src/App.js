b1a606eb3321d10cd1fd126cbaa94b462ee91515
birennicolas <90913609+birennicolas@users.noreply.github.com>
2022-02-01 18:57:39 +0100

    Register done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import './components/Parallax/parallax.css';
 import Home from './pages/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import app from './app.css'
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,7 +11,12 @@ import { useContext, useState } from "react";
 import { AuthContext } from './Context/AuthContext';
 import Walk from './pages/Walk/Walk';
 import Login from './pages/Login/Login';
+import axios from 'axios';
 
+axios.defaults.baseURL = "http://fundait.test/";
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+axios.defaults.withCredentials = true;
 
 
 function App() {
--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,73 +1,84 @@
-import { useRef, useState } from "react";
-import { useHistory } from "react-router";
+import axios from 'axios';
+import React, { useState } from 'react';
+import "./register.css";
+import swal from 'sweetalert';
+import { useHistory } from 'react-router-dom';
 import { style } from "@mui/system";
 import styled from "styled-components";
 
 
-export default function Register({showModal, setShowModal}) {
-    const username = useRef();
-    const email = useRef();
-    const password = useRef();
-    const passwordAgain = useRef();
-    const history = useHistory();
-
-
+export default function Register ({showModal, setShowModal}) {
     
-
-
-
-  
-    const handleClick = async (e) => {
-      e.preventDefault();
-      if (passwordAgain.current.value !== password.current.value) {
-        passwordAgain.current.setCustomValidity("Passwords don't match!");
-      } else {
-        const user = {
-          username: username.current.value,
-          email: email.current.value,
-          password: password.current.value,
-        };
-        try {
-          await //axios.post("/auth/register", user);
-          history.push("/login");
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    };
-
-  
-    return (
-      <>
       
-      {showModal ? <div class="signupFrm">
-      <form action="" class="form">
-        <h1 class="title">Sign up</h1>
-  
-        <div class="inputContainer">
-          <input type="text" class="input" placeholder="a" />
-          <label for="" class="label">Email</label>
-        </div>
-  
-        <div class="inputContainer">
-          <input type="text" class="input" placeholder="a" />
-          <label for="" class="label">Username</label>
-        </div>
-  
-        <div class="inputContainer">
-          <input type="text" class="input" placeholder="a" />
-          <label for="" class="label">Password</label>
-        </div>
-  
-        <div class="inputContainer">
-          <input type="text" class="input" placeholder="a" />
-          <label for="" class="label">Confirm Password</label>
-        </div>
-  
-        <input type="submit" class="submitBtn" value="Sign up" />
-      </form>
-    </div> : null}
+  const history = useHistory();
 
-    </>
-  );
-}
+  const [registerInput, setRegister] = useState({
+    name:'',
+    email:'',
+    password:'',
+    error_list: [],
+  });
+
+  const handleInput = (e) => {
+    e.persist();
+    setRegister({...registerInput, [e.target.name]: e.target.value});
+  }
+
+  const registerSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      username: registerInput.username,
+      email: registerInput.email,
+      password: registerInput.password,
+    }
+
+    axios.get('/sanctum/csrf-cookie').then(response => {
+      axios.post(`/api/register`, data).then(res => {
+        if(res.data.status === 200)
+        {
+            localStorage.setItem('auth_token', res.data.token);
+            localStorage.setItem('auth_name', res.data.username);
+            swal("Success",res.data.message,"success");
+            history.pushState('/home');
+        }
+        else{
+            setRegister({...registerInput, error_list: res.data.validation_errors});
+        }
+        });
+    });
+  }
+
+      return (
+        <>
+      
+        {showModal ?<div className="signupFrm">
+        <form onSubmit={registerSubmit} className="form">
+          <h1 className="title">Sign up</h1>
+    
+          <div class="inputContainer">
+            <input type="" name="username" onChange={handleInput} value={registerInput.username} class="input" placeholder="a" />
+            <label for="" class="label">Username</label>
+          </div>
+          <span>{registerInput.error_list.username}</span>
+
+          <div class="inputContainer">
+            <input type="" name="email" onChange={handleInput} value={registerInput.email} class="input" placeholder="a" />
+            <label for="" class="label">Email</label>
+          </div>
+          <span>{registerInput.error_list.email}</span>
+    
+          <div class="inputContainer">
+            <input type="" name="password" onChange={handleInput} value={registerInput.password} class="input" placeholder="Password" />
+            <label for="" class="label">Password</label>
+          </div>
+          <span>{registerInput.error_list.password}</span>
+    
+          <input type="submit" class="submitBtn" value="Sign up" />
+        </form>
+      </div> : null}
+
+      </>
+      );
+
+  }
