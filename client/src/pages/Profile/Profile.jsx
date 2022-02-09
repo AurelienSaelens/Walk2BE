21ff55a03fdf8c1071eab310fba14d75238e8d12
Saelens Aurelien <aureliensaelens0@gmail.com>
2022-02-09 12:02:39 +0100

    profile page

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import axios from "axios";
 import Topbar from '../../components/Topbar/Topbar';
 import profile from "./profile.css"
+import Api from '../../Api.json'
+import bg from './bg.jpeg'
+import { FcLike } from "react-icons/fc";
+
 
 function Profile() {
+
+    const users = localStorage.getItem('auth_name');
+
+
   return (
       <>
       <body className='profile-ctn'>
       <Topbar />
-      <div className='list'>
+      <div className='block-left'>
         <h2>This is a list with your favorites walks : </h2>
+        <div className="card">
+            <div class="container">
+            <h2>Bruxelles</h2> 
+                <p>Saint pierre</p>
+                <FcLike className="iconLike" />
+            </div>
+        </div>    
         </div>  
+        <div className='block-right'>
+        <h2>Welcome to your profile page : {users}</h2>
+        </div>
       </body>
       </>
       );
