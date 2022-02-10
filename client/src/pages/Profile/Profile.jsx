71caed82a64319272f11c13d0605acf5a3ef4ebd
Saelens Aurelien <aureliensaelens0@gmail.com>
2022-02-10 10:25:50 +0100

    profile page progress

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,92 +1,143 @@
-import React from 'react';
-import axios from "axios";
-import Topbar from '../../components/Topbar/Topbar';
-import profile from "./profile.css"
-import Api from '../../Api.json'
-import bg from './bg.jpeg'
+import React from "react";
+import Topbar from "../../components/Topbar/Topbar";
+import "./profile.css";
 import { FcLike } from "react-icons/fc";
 
-
 function Profile() {
-
-    const users = localStorage.getItem('auth_name');
-
+  const users = localStorage.getItem("auth_name");
 
   return (
-      <>
-      <body className='profile-ctn'>
-      <Topbar />
-      <div className='block-left'>
-        <h2>This is a list with your favorites walks : </h2>
-        <div className="card1">
+    <>
+      <div className="profile-ctn">
+        <div className="block-left">
+          <Topbar />
+          <h2>This is a list with your favorites walks : </h2>
+          <div className="card1">
             <div class="container">
-            <h2>Bruxelles</h2> 
-                <p>Saint pierre</p>
-                <FcLike className="iconLike" />
+              <h2>Bruxelles</h2>
+              <p>Saint pierre</p>
+              <FcLike className="iconLike" />
             </div>
-        </div>   
-        <div className="card1">
-        <div class="container">
-        <h2>Bruxelles</h2> 
-            <p>Saint pierre</p>
-            <FcLike className="iconLike" />
+          </div>
+          <div className="card1">
+            <div class="container">
+              <h2>Bruxelles</h2>
+              <p>Saint pierre</p>
+              <FcLike className="iconLike" />
+            </div>
+          </div>{" "}
+          <div className="card1">
+            <div class="container">
+              <h2>Bruxelles</h2>
+              <p>Saint pierre</p>
+              <FcLike className="iconLike" />
+            </div>
+          </div>{" "}
+          <div className="card1">
+            <div class="container">
+              <h2>Bruxelles</h2>
+              <p>Saint pierre</p>
+              <FcLike className="iconLike" />
+            </div>
+          </div>{" "}
+          <div className="card1">
+            <div class="container">
+              <h2>Bruxelles</h2>
+              <p>Saint pierre</p>
+              <FcLike className="iconLike" />
+            </div>
+          </div>{" "}
+          <div className="card1">
+            <div class="container">
+              <h2>Bruxelles</h2>
+              <p>Saint pierre</p>
+              <FcLike className="iconLike" />
+            </div>
+          </div>{" "}
+          <div className="card1">
+            <div class="container">
+              <h2>Bruxelles</h2>
+              <p>Saint pierre</p>
+              <FcLike className="iconLike" />
+            </div>
+          </div>{" "}
+          <div className="card1">
+            <div class="container">
+              <h2>Bruxelles</h2>
+              <p>Saint pierre</p>
+              <FcLike className="iconLike" />
+            </div>
+          </div>{" "}
+          <div className="card1">
+            <div class="container">
+              <h2>Bruxelles</h2>
+              <p>Saint pierre</p>
+              <FcLike className="iconLike" />
+            </div>
+          </div>{" "}
+          <div className="card1">
+            <div class="container">
+              <h2>Bruxelles</h2>
+              <p>Saint pierre</p>
+              <FcLike className="iconLike" />
+            </div>
+          </div>
         </div>
-    </div>         <div className="card1">
-    <div class="container">
-    <h2>Bruxelles</h2> 
-        <p>Saint pierre</p>
-        <FcLike className="iconLike" />
-    </div>
-</div>         <div className="card1">
-<div class="container">
-<h2>Bruxelles</h2> 
-    <p>Saint pierre</p>
-    <FcLike className="iconLike" />
-</div>
-</div>         <div className="card1">
-<div class="container">
-<h2>Bruxelles</h2> 
-    <p>Saint pierre</p>
-    <FcLike className="iconLike" />
-</div>
-</div>         <div className="card1">
-<div class="container">
-<h2>Bruxelles</h2> 
-    <p>Saint pierre</p>
-    <FcLike className="iconLike" />
-</div>
-</div>         <div className="card1">
-<div class="container">
-<h2>Bruxelles</h2> 
-    <p>Saint pierre</p>
-    <FcLike className="iconLike" />
-</div>
-</div>         <div className="card1">
-<div class="container">
-<h2>Bruxelles</h2> 
-    <p>Saint pierre</p>
-    <FcLike className="iconLike" />
-</div>
-</div>         <div className="card1">
-<div class="container">
-<h2>Bruxelles</h2> 
-    <p>Saint pierre</p>
-    <FcLike className="iconLike" />
-</div>
-</div>         <div className="card1">
-<div class="container">
-<h2>Bruxelles</h2> 
-    <p>Saint pierre</p>
-    <FcLike className="iconLike" />
-</div>
-</div>  
-        </div>  
-        <div className='block-right'>
-        <h2>Welcome to your profile page : {users}</h2>
+        <div className="fixed">
+          <div className="block-right">
+            <h2>
+              Welcome to your profile page  <br></br>
+              <b>AurelienSa{users}</b>
+            </h2>
+            <div class="profile-pic">
+            <span class="glyphicon glyphicon-camera"></span>
+            <span>Change Image</span>
         </div>
-      </body>
-      </>
-      );
-}  
+        <div className="profileFrm">
+        <form  className="formProfile">
+
+
+          <div class="inputContainer">
+            <input
+              type=""
+              name="username"
+              class="input"
+              placeholder="a"
+            />
+            <label for="" class="label">
+              Username
+            </label>
+          </div>
+
+          <div class="inputContainer">
+            <input
+              type=""
+              name="email"
+              class="input"
+              placeholder="a"
+            />
+            <label for="" class="label">
+              Email
+            </label>
+          </div>
+          <div class="inputContainer">
+            <input
+              type="password"
+              name="password"
+              class="input"
+              placeholder="Password"
+            />
+            <label for="" class="label">
+              Password
+            </label>
+          </div>
+        </form>
+      </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Profile;
