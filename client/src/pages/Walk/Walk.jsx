561365ae112f8e4979fba34043b2a8d7b6bf61e4
Saelens Aurelien <aureliensaelens0@gmail.com>
2022-02-13 10:08:47 +0100

    test geo

--- client/src/pages/Walk/Walk.jsx
+++ client/src/pages/Walk/Walk.jsx
@@ -3,10 +3,12 @@ import Topbar from "../../components/Topbar/Topbar";
 import Fetchapi from "../../components/FetchApi/Fetchapi";
 
 function Walk() {
+  
+
+
   return (
     <div className="walk-content">
-    <Topbar />
-      <Fetchapi />
+    <Fetchapi />
     </div>
   );
 }
