e893729ee50f19f527e1ffd1559d74548fd01f4d
Saelens Aurelien <aureliensaelens0@gmail.com>
2022-02-14 16:07:53 +0100

    try some styles

--- client/src/pages/Walk/Walk.jsx
+++ client/src/pages/Walk/Walk.jsx
@@ -3,12 +3,10 @@ import Topbar from "../../components/Topbar/Topbar";
 import Fetchapi from "../../components/FetchApi/Fetchapi";
 
 function Walk() {
-  
-
-
   return (
     <div className="walk-content">
-    <Fetchapi />
+    <Topbar />
+      <Fetchapi />
     </div>
   );
 }
