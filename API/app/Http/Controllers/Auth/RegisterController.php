7a2c922081297586476c0c5d7ff27d4fa89e1924
Saelens Aurelien <aureliensaelens0@gmail.com>
2022-02-15 16:12:24 +0100

    update for age of people

--- API/app/Http/Controllers/Auth/RegisterController.php
+++ API/app/Http/Controllers/Auth/RegisterController.php
@@ -55,7 +55,7 @@ class RegisterController extends Controller
             'gender'=> ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'old' => ['required', 'string',]
+            'old' => ['required', 'string', 'max:255']
         ]);
     }
 
